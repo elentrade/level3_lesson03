1a76057c803b58be58e920726724a8da282e5aa2
Elena <elentrade@rambler.ru>
2021-03-29 07:16:28 +0400

    задание 1 - папка ex1, задание 2 - папка ex2. задание 3 - папка ex3

--- src/ex1/Main1.java
+++ src/ex1/Main1.java
@@ -11,40 +11,40 @@ public class Main1 {
 
 
 //   ex # 1 1-st way
-	try (FileInputStream in = new FileInputStream("docs50.xml")) {
-	    int i;
-	    while ((i=in.read())!=-1){
-            System.out.print((char) i);
-        }
-    } catch (FileNotFoundException e) {
-        e.printStackTrace();
-    } catch (IOException e) {
-        e.printStackTrace();
-    }
+//	try (FileInputStream in = new FileInputStream("docs50.xml")) {
+//	    int i;
+//	    while ((i=in.read())!=-1){
+//            System.out.print((char) i);
+//        }
+//    } catch (FileNotFoundException e) {
+//        e.printStackTrace();
+//    } catch (IOException e) {
+//        e.printStackTrace();
+//    }
 
 
         //   ex # 1 2-nd way
-//        BufferedReader br = null;
-//        FileReader fr = null;
-//        String currentLine;
-//        try {
-//            fr = new FileReader("docs50.xml");
-//            br = new BufferedReader(fr);
-//            while ((currentLine= br.readLine())!=null){
-//                System.out.println(currentLine);
-//
-//            }
-//        } catch (FileNotFoundException e) {
-//            e.printStackTrace();
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//        try {
-//            br.close();
-//            fr.close();
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
+        BufferedReader br = null;
+        FileReader fr = null;
+        String currentLine;
+        try {
+            fr = new FileReader("docs50.xml");
+            br = new BufferedReader(fr);
+            while ((currentLine= br.readLine())!=null){
+                System.out.println(currentLine);
+
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            br.close();
+            fr.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
 
     }
